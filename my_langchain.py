22f257a2652f5f5742e629f8317bde15c5aaac4b
Chamchis <junhoo88888@gmail.com>
2024-03-13 16:40:24 +0900

    langchain_test

--- my_langchain.py
+++ my_langchain.py
@@ -25,3 +25,24 @@ print(chain)
 
 print(chain.invoke({'input':'2024년 청년 정책에 대하여 알려줘'}))
 
+class square_numbers:
+    def __ror__(self, other):
+        return self(other)
+    def __call__(self, vals):
+        self.vals = [n ** 2 for n in vals]
+        return self.vals
+
+class sum_numbers:
+    def __ror__(self, other):
+        return self(other)
+    def __call__(self, vals):
+      return sum(vals)
+
+
+# 숫자 리스트
+numbers = [1, 2, 3, 4]
+sn = square_numbers()
+sum_n = sum_numbers()
+# 파이프 연산자를 사용하여 연산 진행
+result =numbers | sn | sum_n
+result
